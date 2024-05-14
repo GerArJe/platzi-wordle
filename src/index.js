90c261d66d489ed17383ac0282115eaec0da8fec
German Arevalo J <germanarevalojerez@gmail.com>
2024-05-14 14:37:54 -0500

    feat: :sparkles: se agrega funcionalidad de reiniciar el juego

--- src/index.js
+++ src/index.js
@@ -1,17 +1,18 @@
-import { filter, fromEvent, map, Subject, takeUntil } from "rxjs";
+import { merge, fromEvent, Subject } from "rxjs";
+import { map, filter, takeUntil } from "rxjs/operators";
 import WORDS_LIST from "./wordsList.json";
 
 const restartButton = document.getElementById("restart-button");
 const letterRows = document.getElementsByClassName("letter-row");
-const onKeyDown$ = fromEvent(document, "keydown");
 const messageText = document.getElementById("message-text");
-let letterIndex = 0;
-let letterRowIndex = 0;
-let userAnswer = [];
+const onKeyDown$ = fromEvent(document, "keydown");
+let letterIndex;
+let letterRowIndex;
+let userAnswer;
 const getRandomWord = () =>
   WORDS_LIST[Math.floor(Math.random() * WORDS_LIST.length)];
-let rightWord = getRandomWord();
-console.log("🚀 ~ rightWord:", rightWord);
+let rightWord;
+
 const userWinOrLoose$ = new Subject();
 
 const insertLetter$ = onKeyDown$.pipe(
@@ -21,6 +22,7 @@ const insertLetter$ = onKeyDown$.pipe(
       pressedKey.length === 1 && pressedKey.match(/[a-z]/i) && letterIndex < 5
   )
 );
+
 const insertLetter = {
   next: (letter) => {
     let letterBox =
@@ -32,29 +34,25 @@ const insertLetter = {
   },
 };
 
-const removeLetter$ = onKeyDown$.pipe(
-  map((event) => event.key),
-  filter((key) => key === "Backspace" && letterIndex !== 0)
-);
-const deleteLetter = {
-  next: () => {
-    let letterBox = letterRows[letterRowIndex].children[userAnswer.length - 1];
-    letterBox.textContent = "";
-    letterBox.classList = "letter";
-    letterIndex--;
-    userAnswer.pop();
-  },
-};
-
 const checkWord$ = onKeyDown$.pipe(
   map((event) => event.key),
-  filter((key) => key === "Enter" && letterIndex === 5 && letterRowIndex <= 5)
+  filter((key) => key === "Enter" && letterRowIndex < 6)
 );
+
 const checkWord = {
   next: () => {
-    if (userAnswer.length !== 5) {
-      messageText.textContent = "¡Te faltan algunas letras!";
+    if (userAnswer.length != 5) {
+      messageText.textContent =
+        userAnswer.length === 4
+          ? "Te falta 1 letra"
+          : `Te faltan ${5 - userAnswer.length} letras`;
       return;
+    }
+
+    if (!WORDS_LIST.includes(userAnswer.join(""))) {
+      messageText.textContent = `¡La palabra ${userAnswer
+        .join("")
+        .toUpperCase()} no está en la lista!`;
     }
 
     userAnswer.map((_, i) => {
@@ -93,14 +91,50 @@ const checkWord = {
   },
 };
 
-userWinOrLoose$.subscribe({
+const removeLetter$ = onKeyDown$.pipe(
+  map((event) => event.key),
+  filter((key) => key === "Backspace" && letterIndex !== 0)
+);
+
+const removeLetter = {
   next: () => {
-    let letterRowsWinned = Array.from(letterRows)[letterRowIndex];
-    for (let index = 0; index < 5; index++) {
-      letterRowsWinned.children[index].classList.add("letter-green");
-    }
+    let letterBox = letterRows[letterRowIndex].children[userAnswer.length - 1];
+    letterBox.textContent = "";
+    letterBox.classList = "letter";
+    letterIndex--;
+    userAnswer.pop();
   },
+};
+
+const onRestartClick$ = fromEvent(restartButton, "click");
+const onWindowLoad$ = fromEvent(window, "load");
+const restartGame$ = merge(onWindowLoad$, onRestartClick$);
+
+restartGame$.subscribe(() => {
+  Array.from(letterRows).map((row) =>
+    Array.from(row.children).map((letterBox) => {
+      letterBox.textContent = "";
+      letterBox.classList = "letter";
+    })
+  );
+
+  letterRowIndex = 0;
+  letterIndex = 0;
+  userAnswer = [];
+  messageText.textContent = "";
+  rightWord = getRandomWord();
+
+  restartButton.disabled = true;
+
+  console.log(`Right word: ${rightWord}`);
+
+  let insertLetterSubscription = insertLetter$
+    .pipe(takeUntil(userWinOrLoose$))
+    .subscribe(insertLetter);
+  let checkWordSubscription = checkWord$
+    .pipe(takeUntil(userWinOrLoose$))
+    .subscribe(checkWord);
+  let removeLetterSubscription = removeLetter$
+    .pipe(takeUntil(userWinOrLoose$))
+    .subscribe(removeLetter);
 });
-insertLetter$.pipe(takeUntil(userWinOrLoose$)).subscribe(insertLetter);
-checkWord$.pipe(takeUntil(userWinOrLoose$)).subscribe(checkWord);
-removeLetter$.pipe(takeUntil(userWinOrLoose$)).subscribe(deleteLetter);
